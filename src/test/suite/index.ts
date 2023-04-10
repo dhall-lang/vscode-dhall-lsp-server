e64b5c61dfe0053504bfd6c8e8f665db7f5b267b
kands-code <kands-code@qq.com>
2023-04-11 03:16:07 +0800

    fix: use webpack

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,38 +1,38 @@
-import * as path from "path";
-import * as Mocha from "mocha";
 import * as glob from "glob";
+import * as Mocha from "mocha";
+import * as path from "path";
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
+    timeout: 90000,
     color: true,
   });
 
   const testsRoot = path.resolve(__dirname, "..");
 
   return new Promise((c, e) => {
-    glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
+    glob
+      .glob("**/**.test.js", { cwd: testsRoot })
+      .then((files) => {
+        // Add files to the test suite
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        console.error(err);
-        e(err);
-      }
-    });
+        try {
+          // Run the mocha test
+          mocha.run((failures) => {
+            if (failures > 0) {
+              e(new Error(`${failures} tests failed.`));
+            } else {
+              c();
+            }
+          });
+        } catch (err) {
+          console.error(err);
+          e(err);
+        }
+      })
+      .catch((err) => e(err));
   });
 }
